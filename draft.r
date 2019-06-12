92ba1845c4554ac57e27f79d5ef5ca57298f40ea
demydd <demydd@gmail.com>
2019-06-13 00:22:38 +0300

    R file compiled. Rnotebook file is in progress.

--- draft.r
+++ draft.r
@@ -10,7 +10,7 @@ data_all <- data.table(read.csv(paste(location, filename, sep = "")))
 vars <- c('AMT_INCOME_TOTAL',	'AMT_CREDIT',	'AMT_ANNUITY',	'AMT_GOODS_PRICE',	'REGION_POPULATION_RELATIVE',	'HOUR_APPR_PROCESS_START')
 data_vars <- data_all[, ..vars]
 
-data_vars$AMT_ANNUITY[!is.na(data_vars$AMT_ANNUITY)] <- 1
+#data_vars$AMT_ANNUITY[!is.na(data_vars$AMT_ANNUITY)] <- 1
 
 gm <- function(x){  x <- x[!is.na(x)]; exp(mean(log(x)))} #https://www.r-bloggers.com/r-geometric-mean/
 hm <- function(x){harmonic.mean(x, na.rm=TRUE)} #https://www.r-bloggers.com/geometric-and-harmonic-means-in-r/
@@ -40,22 +40,16 @@ boxplot(data_vars$AMT_CREDIT,
         col="orange",
         border="brown"
 )
+dev.off()
 
 #variance
-range <- min_max[2] - min_max[1]
-d <- data_vars$AMT_INCOME_TOTAL - mean_summary$AMT_INCOME_TOTAL #[1, ..vars]
-
 qty <- dim(data_vars)[1]
-d <- apply(as.data.table(vars), 1,
-            function(x){
-                        sum(abs(data_vars[, ..x] -  unlist(mean_summary[1,..x])))/qty
-                       }
-           )
+range <- min_max[2] - min_max[1]
+d <- apply(as.data.table(vars), 1, function(x){sum(abs(data_vars[as.vector(!is.na(data_vars[, ..x])), ..x] - unlist(mean_summary[1,..x]))) / sum(!is.na(data_vars[, ..x]))})
+stanDev <- apply(as.data.table(vars), 1, function(x){sd(unlist(data_vars[as.vector(!is.na(data_vars[, ..x])), ..x]))})
 
-x <- vars[1]
-sum(abs(data_vars[,..x] - unlist(rep(mean_summary[1,..x],step))))
+Oscillator_ratio <- range/mean_summary[1, ..vars] * 100
+Linear_variance_ratio <- d/mean_summary[1, ..vars] * 100
+Variance_ration <- stanDev/mean_summary[1, ..vars] * 100
 
-sum(abs(data_vars[,..x] - unlist(mean_summary[1,..x])))/step
 
-class(data_vars[, ..x])  
-class(mean_summary[, ..x])
